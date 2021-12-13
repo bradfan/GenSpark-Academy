c371fd9703bccbd43955328812e8ca34d65bdc5f
bradfan <bradley.tomlinson@genspark.com>
2021-12-13 13:49:14 -0700

    Create test files for dragon cave and guess the number

---
+++ genspark/dragon-cave/src/test/java/com/company/MainTest.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    Main main;
+
+    @BeforeEach
+    void setUp() {
+        main = new Main();
+    }
+
+    @DisplayName("Test for User Response")
+    @Test
+    void userResponse() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+}
