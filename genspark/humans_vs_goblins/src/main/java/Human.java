bfb309375914d3f0e282119640b6bb6ecaac8551
bradfan <bradley.tomlinson@genspark.com>
2021-12-29 10:00:15 -0700

    Set up attributes, constructor, setters and getters Human

--- genspark/humans_vs_goblins/src/main/java/Human.java
+++ genspark/humans_vs_goblins/src/main/java/Human.java
@@ -1,2 +1,62 @@
+
+
 public class Human {
+    private boolean isAlive;
+    private int location;
+    private int hitPoints = 20;
+    private int offense = 20;
+    private int defense = 10;
+
+//default constructor
+    public Human() {
+
+    }
+//    parameterized constructor
+    public Human(int hitPoints, int offense, int defense) {
+        this.hitPoints = hitPoints;
+        this.offense = offense;
+        this.defense = defense;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void setLocation(int location) {
+        this.location = location;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getOffense() {
+        return offense;
+    }
+
+    public void setOffense(int offense) {
+        this.offense = offense;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
 }
+
+
