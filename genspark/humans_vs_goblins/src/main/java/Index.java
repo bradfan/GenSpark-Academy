7fd260bab3ac753e40e6f66dec84011e9d40a3cc
bradfan <bradley.tomlinson@genspark.com>
2021-12-30 07:59:22 -0700

    Print Grid to console Index

--- genspark/humans_vs_goblins/src/main/java/Index.java
+++ genspark/humans_vs_goblins/src/main/java/Index.java
@@ -1,10 +1,29 @@
 
 
 public class Index {
+
     public static void main(String[] args) {
         Land land = new Land();
         Human human = new Human();
         Goblin goblin = new Goblin();
+        int [][] grid = {{10, 11, 12, 13, 14},
+                         {15, 16, 17, 18, 19},
+                         {20, 21, 22, 23, 24},
+                         {25, 26, 27, 28, 29},
+                         {30, 31, 32, 34, 35}};
+
+        for(int rows = 0; rows < 5; rows++) {
+            System.out.println();
+            for(int cols = 0; cols < 5; cols++) {
+                System.out.print(grid[rows][cols] + " ");
+            }
+            System.out.println();
+        }
+
+
+
+
+
 
     }
 }
--- genspark/humans_vs_goblins/src/main/java/Land.java
+++ genspark/humans_vs_goblins/src/main/java/Land.java
@@ -7,22 +7,5 @@ public class Land {
        }
 
 
-    public void createGrid(ArrayList<Object> grid) {
-
-//        String newGrid[][] = new String[][] {{"[]", "[]", "[]", "[]", "[]"},
-//                                            {"[]", "[]", "[]", "[]", "[]"},
-//                                            {"[]", "[]", "[]", "[]", "[]"},
-//                                            {"[]", "[]", "[]", "[]", "[]"},
-//                                            {"[]", "[]", "[]", "[]", "[]"}};
-
-        for(int rows = 0; rows < 5; rows++) {
-                System.out.println(grid.get(rows));
-                for(int cols = 0; cols < 5; cols++) {
-                    System.out.print(grid.get(cols) + " ");
-                }
-            System.out.println();
-            }
-
-    }
-
+       
 }
