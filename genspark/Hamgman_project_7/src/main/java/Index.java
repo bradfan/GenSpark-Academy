bc2c400b99b07f5c503bd4991ad0e3b9c7462c82
bradfan <bradley.tomlinson@genspark.com>
2022-01-04 14:49:51 -0700

    Add display file java

--- genspark/Hamgman_project_7/src/main/java/Index.java
+++ genspark/Hamgman_project_7/src/main/java/Index.java
@@ -1,9 +1,32 @@
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
 
 //hover over Index and right click to generate test file
 public class Index {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+     var a = Files.lines(Paths.get("hangman_display.text"));
+     Files.write(Paths.get("C:\\GenSpark-Academy\\genspark\\Hamgman_project_7\\src\\main\\java\\hangman_display.text"), Collections.singleton("Hello"));
+
+
+
+
+
+
+
+
+
 
     }
+//    methods go here outside of the main in the class
+
+
+
+
+
 }
+
+
