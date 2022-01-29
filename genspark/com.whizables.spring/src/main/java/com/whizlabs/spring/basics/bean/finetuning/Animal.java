05720f44fedeb947651948fc30af013a3eb2c5c9
bradfan <bradley.tomlinson@genspark.com>
2022-01-29 16:46:35 -0700

    Autowiring Fine Tuning, whizables

---
+++ genspark/com.whizables.spring/src/main/java/com/whizlabs/spring/basics/bean/finetuning/Animal.java
@@ -0,0 +1,6 @@
+package com.whizlabs.spring.basics.bean.finetuning;
+
+public interface Animal {
+    String move();
+
+}
---
+++ genspark/com.whizables.spring/src/main/java/com/whizlabs/spring/basics/bean/finetuning/FineTuneConfig.java
@@ -0,0 +1,8 @@
+package com.whizlabs.spring.basics.bean.finetuning;
+
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+public class FineTuneConfig {
+}
