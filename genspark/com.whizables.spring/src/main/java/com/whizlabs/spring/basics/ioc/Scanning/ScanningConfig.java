28887381c1be1325956e1c5174835188360b8677
bradfan <bradley.tomlinson@genspark.com>
2022-01-29 14:42:41 -0700

    Dependency Injection and Autowiring, whizables

---
+++ genspark/com.whizables.spring/src/main/java/com/whizlabs/spring/basics/ioc/Scanning/ScanningConfig.java
@@ -0,0 +1,8 @@
+package com.whizlabs.spring.basics.ioc.Scanning;
+
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.whizlabs.spring.basics.ioc.scanning")
+public class ScanningConfig {
+}
