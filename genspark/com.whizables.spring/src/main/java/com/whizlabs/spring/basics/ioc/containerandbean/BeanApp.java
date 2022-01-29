a2e98c5e59f462799ccdacab91f212987e7170c9
bradfan <bradley.tomlinson@genspark.com>
2022-01-29 12:40:36 -0700

    Spring IoC part II, whizables

--- genspark/com.whizables.spring/src/main/java/com/whizlabs/spring/basics/ioc/containerandbean/BeanApp.java
+++ genspark/com.whizables.spring/src/main/java/com/whizlabs/spring/basics/ioc/containerandbean/BeanApp.java
@@ -1,6 +1,7 @@
 package com.whizlabs.spring.basics.ioc.containerandbean;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class BeanApp {
@@ -9,5 +10,13 @@ public class BeanApp {
         Person john = (Person) contextWithXmlConfig.getBean("person");
         System.out.println(john.getFullName());
         System.out.println(john.getAddress());
-    }
+
+        ApplicationContext contextWithJavaConfig = new AnnotationConfigApplicationContext(BeanConfig.class);
+        Address address1 = (Address) contextWithJavaConfig.getBean("address");
+        Address address2 =  contextWithJavaConfig.getBean("kingAvenue", Address.class);
+        System.out.println(address1 == address2);
+        Person jane = contextWithJavaConfig.getBean(Person.class);
+        System.out.println(jane.getFullName());
+        System.out.println(jane.getAddress());
+     }
 }
