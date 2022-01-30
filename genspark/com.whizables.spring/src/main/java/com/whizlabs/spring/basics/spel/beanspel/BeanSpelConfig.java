7ebd6f7c4ba5f4e8c80b01629a03c3a4542fee8c
bradfan <bradley.tomlinson@genspark.com>
2022-01-30 15:36:11 -0700

    File not in correct directory, whizlabs

---
+++ genspark/com.whizables.spring/src/main/java/com/whizlabs/spring/basics/spel/beanspel/BeanSpelConfig.java
@@ -0,0 +1,9 @@
+package com.whizlabs.spring.basics.spel.beanspel;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+
+@ComponentScan
+@PropertySource("resources\bean-spel.properties")
+public class BeanSpelConfig {
+}
