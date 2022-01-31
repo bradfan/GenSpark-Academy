df70041d353860225da10f4fc2abac6013afa0ed
bradfan <bradley.tomlinson@genspark.com>
2022-01-31 11:54:41 -0700

    Refactoring/practice sandbox

--- genspark/sandbox/src/java/ChallengeStorage.java
+++ genspark/sandbox/src/java/ChallengeStorage.java
@@ -4,43 +4,52 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ChallengeStorage {
 
-<<<<<<< HEAD
-
     public static void writeOutputOneFile() throws IOException {
         var getInputOne = Files.lines(Paths.get("out/production/sandbox/file_practice.txt")).collect(Collectors.toList());
         System.out.println(getInputOne);
-        int lastIndex = getInputOne.size();
-        System.out.println(lastIndex);
-        int halfwayIndex = (lastIndex / 2);
-        if(getInputOne.size() % 2 == 0 == false) {
-               halfwayIndex += 1;
+        List<String> append = new ArrayList<>();
+        int i = 0;
+        while(i <= getInputOne.size()-1){
+            if(getInputOne.size() > (i + 4)) {
+                append.add(getInputOne.get(i));
+            } else {
+                append.add(getInputOne.get(i) + " " + getInputOne.get(i + 4));
+            }
+            System.out.println("Append: " + append);
+            i++;
         }
-        System.out.println(halfwayIndex);
 
-        List<String> together = new ArrayList<>();
-//        together = getInputOne.stream().reduce("", (e1,e2) -> )
-
-
-        System.out.println(together);
-        var outputOne = Files.write(Paths.get("output_one.txt"), together, StandardCharsets.UTF_8,
+        System.out.println(append);
+        var outputOne = Files.write(Paths.get("output_one.txt"), append, StandardCharsets.UTF_8,
                 StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+
     }
 
-//    List<String> a = new ArrayList<>();
-//            List<String> b = new ArrayList<>();
-//            for (int i = 0; i <= halfwayIndex; i++) {
-//                a.add(getInputOne.get(i));
+//    int counter = 0;
+//        while(counter <= getInputOne.size() - 1) {
+//        append.add(getInputOne.get(counter));
+//            if(getInputOne.size() < counter + 3) {
+//        append.add(getInputOne.get(counter + 3));
 //            }
-//        System.out.println("A: " + a);
-//            for (int i = halfwayIndex + 1; i <= lastIndex; i++) {
-//                b.add(getInputOne.get(i));
-//            }
-//      System.out.println("B: " + b);
+//        counter ++;
+//    }
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        writeOutputOneFile();
+
+    }
+}
+
 
 
 //        public static void writeOutputTwoFile() throws IOException {
@@ -63,57 +72,49 @@ public class ChallengeStorage {
 //                    StandardOpenOption.CREATE, StandardOpenOption.APPEND);
 //        }
 
-    public static void main(String[] args) throws IOException {
 
-        writeOutputOneFile();
-
-
-=======
-
-
-
-    public void writeOutputOneFile() throws IOException {
-            var getInputOne = Files.lines(Paths.get("input/one_to_seven.txt")).collect(Collectors.toList());
-            System.out.println(getInputOne);
-            int halfwayIndex = (getInputOne.size() / 2);
-            int lastIndex = getInputOne.size() - 1;
-            List<String> a = new ArrayList<>();
-            List<String> b = new ArrayList<>();
-            for (int i = 0; i < halfwayIndex; i++) {
-                a.add(getInputOne.get(i));
-            }
-            for (int i = halfwayIndex; i <= lastIndex; i++) {
-                b.add(getInputOne.get(i));
-            }
-            List<String> together = new ArrayList<>();
-            for (int i = 0; i <= a.size() - 1; i++) {
-                together.add(a.get(i) + " " + b.get(i));
-            }
+//    public void writeOutputOneFile() throws IOException {
+//            var getInputOne = Files.lines(Paths.get("input/one_to_seven.txt")).collect(Collectors.toList());
+//            System.out.println(getInputOne);
+//            int halfwayIndex = (getInputOne.size() / 2);
+//            int lastIndex = getInputOne.size() - 1;
+//            List<String> a = new ArrayList<>();
+//            List<String> b = new ArrayList<>();
+//            for (int i = 0; i < halfwayIndex; i++) {
+//                a.add(getInputOne.get(i));
+//            }
+//            for (int i = halfwayIndex; i <= lastIndex; i++) {
+//                b.add(getInputOne.get(i));
+//            }
+//            List<String> together = new ArrayList<>();
+//            for (int i = 0; i <= a.size() - 1; i++) {
+//                together.add(a.get(i) + " " + b.get(i));
+//            }
 //        var outputOne = Files.write(Paths.get("output_one.txt"), together, StandardCharsets.UTF_8,
 //                StandardOpenOption.APPEND);
-        }
+//        }
 
-        public static void writeOutputTwoFile() throws IOException {
-            var getInputTwo = Files.lines(Paths.get("input/one_to_twenty_five.txt")).collect(Collectors.toList());
-            System.out.println(getInputTwo);
-            List<String> a = new ArrayList<>();
-            List<String> b = new ArrayList<>();
-            List<String> c = new ArrayList<>();
-            List<String> d = new ArrayList<>();
-            List<String> e = new ArrayList<>();
-            List<String> f = new ArrayList<>();
-
-            int start = 0;
-            int counter = 0;
-
-            while(start < 8) {
-                f.add(getInputTwo.get(counter));
-                counter += 9;
-                if(counter > getInputTwo.size() - 1) {
-                    start ++;
-                    counter = start;
-                }
-            }
+//        public static void writeOutputTwoFile() throws IOException {
+//            var getInputTwo = Files.lines(Paths.get("input/one_to_twenty_five.txt")).collect(Collectors.toList());
+//            System.out.println(getInputTwo);
+//            List<String> a = new ArrayList<>();
+//            List<String> b = new ArrayList<>();
+//            List<String> c = new ArrayList<>();
+//            List<String> d = new ArrayList<>();
+//            List<String> e = new ArrayList<>();
+//            List<String> f = new ArrayList<>();
+//
+//            int start = 0;
+//            int counter = 0;
+//
+//            while(start < 8) {
+//                f.add(getInputTwo.get(counter));
+//                counter += 9;
+//                if(counter > getInputTwo.size() - 1) {
+//                    start ++;
+//                    counter = start;
+//                }
+//            }
 
 //            for (int i = 0; i <= getInputTwo.size() - 1; i += 9) {
 //                a.add(getInputTwo.get(i));
@@ -158,22 +159,43 @@ public class ChallengeStorage {
 //                f.add(getInputTwo.get(i));
 //            }
 
-            System.out.println("A: " + a);
-            System.out.println("B: " + b);
-            System.out.println("C: " + c);
-            System.out.println("D: " + d);
-            System.out.println("E: " + e);
-            System.out.println("F: " + f);
+//            System.out.println("A: " + a);
+//            System.out.println("B: " + b);
+//            System.out.println("C: " + c);
+//            System.out.println("D: " + d);
+//            System.out.println("E: " + e);
+//            System.out.println("F: " + f);
 
-            var outputTwo = Files.write(Paths.get("output_two.txt"), f, StandardCharsets.UTF_8,
-                    StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-        }
-
-    public static void main(String[] args) throws IOException {
-
-         writeOutputTwoFile();
->>>>>>> main
-    }
+//            var outputTwo = Files.write(Paths.get("output_two.txt"), f, StandardCharsets.UTF_8,
+//                    StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+//        }
 
 
-}
+//}
+
+
+//        int lastIndex = getInputOne.size();
+//        System.out.println(lastIndex);
+//        int halfwayIndex = (lastIndex / 2);
+////        if (getInputOne.size() % 2 == 0 == false) {
+////            halfwayIndex += 1;
+////        }
+//        System.out.println(halfwayIndex);
+//        List<String> a = new ArrayList<>();
+//        List<String> b = new ArrayList<>();
+//        for (int i = 0; i < halfwayIndex; i++) {
+//            a.add(getInputOne.get(i));
+//        }
+//        System.out.println("A: " + a);
+//        for (int i = halfwayIndex; i < lastIndex; i++) {
+//            b.add(getInputOne.get(i));
+//        }
+//        System.out.println("A: " + a);
+//        System.out.println("B: " + b);
+//        List<String> together = new ArrayList<>();
+//        for (int i = 0; i < getInputOne.size(); i++) {
+//            together.add(a.get(i) + " " + b.get(i));
+//        }
+//
+
+//    }
