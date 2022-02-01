cf320b766b50114d00eaac731b042d5fca94ba73
bradfan <bradley.tomlinson@genspark.com>
2022-02-01 09:24:34 -0700

    Advice Parameters (double check for error), whizlabs

---
+++ genspark/com.whizables.spring/src/main/java/com/whizlabs/spring/basics/aop/parameter/ParameterConfig.java
@@ -0,0 +1,10 @@
+package com.whizlabs.spring.basics.aop.parameter;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@ComponentScan
+@EnableAspectJAutoProxy
+public class ParameterConfig {
+}
