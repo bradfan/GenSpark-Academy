625b58651c76384fdd8c73f854ad7c4a9167bf22
bradfan <bradley.tomlinson@genspark.com>
2022-02-01 13:53:04 -0700

    Init commit Initializr, car parts

---
+++ genspark/car_parts/src/test/java/com/car_parts_assignment/car_parts/CarPartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car_parts_assignment.car_parts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
