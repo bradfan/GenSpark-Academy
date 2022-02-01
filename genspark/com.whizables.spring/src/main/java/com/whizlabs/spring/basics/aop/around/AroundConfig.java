01e3ae497c165f4b137ed403c1ffaa961f63b30e
bradfan <bradley.tomlinson@genspark.com>
2022-02-01 08:52:13 -0700

    Advice Parameters

---
+++ genspark/com.whizables.spring/src/main/java/com/whizlabs/spring/basics/aop/around/AroundConfig.java
@@ -0,0 +1,10 @@
+package com.whizlabs.spring.basics.aop.around;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+@ComponentScan
+@EnableAspectJAutoProxy
+public class AroundConfig {
+}
