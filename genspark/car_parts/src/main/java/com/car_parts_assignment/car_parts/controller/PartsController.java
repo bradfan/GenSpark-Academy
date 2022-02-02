1bc9bf18956abb3ab4beee4923d3223155d4738c
bradfan <bradley.tomlinson@genspark.com>
2022-02-02 09:23:44 -0700

    Create Controller

--- genspark/car_parts/src/main/java/com/car_parts_assignment/car_parts/controller/PartsController.java
+++ genspark/car_parts/src/main/java/com/car_parts_assignment/car_parts/controller/PartsController.java
@@ -1,9 +1,13 @@
 package com.car_parts_assignment.car_parts.controller;
 
 
+import com.car_parts_assignment.car_parts.entity.Part;
 import com.car_parts_assignment.car_parts.service.PartsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class PartsController {
@@ -11,5 +15,36 @@ public class PartsController {
     private final PartsService partsService;
 
     @Autowired
+    public PartsController(@Qualifier("partsServiceIMPL") PartsService partsService) {
+        this.partsService = partsService;
+    }
+
+    //http://localhost:8080/retrieveAllParts
+    @GetMapping("/retrieveAllParts")
+    public List<Part> findAll() {
+        return partsService.findAll();
+    }
+
+    //http://localhost:8080/addPart
+    @PostMapping("/addPart")
+    public Part addPart(@RequestBody Part thePart) {
+        thePart.setId(0);
+        partsService.saveOrUpdate(thePart);
+        return thePart;
+    }
+
+    //http://localhost:8080/updatePart
+    @PutMapping("/updatePart")
+    public Part updatePart(@RequestBody Part updatePart) {
+        partsService.saveOrUpdate(updatePart);
+        return updatePart;
+    }
+
+    //http://localhost:8080/deletePart/1
+    @DeleteMapping("/deleteEmployee/{partId}")
+    public String deletePart(@PathVariable int partId) {
+        partsService.deleteById(partId);
+        return "Deleted part ID: " + partId;
+    }
 
 }
