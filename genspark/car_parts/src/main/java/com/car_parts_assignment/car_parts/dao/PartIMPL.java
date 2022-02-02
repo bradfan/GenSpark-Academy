d77b7bc194ebd5e9577c992be3852c8077e889b3
bradfan <bradley.tomlinson@genspark.com>
2022-02-02 09:49:09 -0700

    Create PartIMPL

--- genspark/car_parts/src/main/java/com/car_parts_assignment/car_parts/dao/PartIMPL.java
+++ genspark/car_parts/src/main/java/com/car_parts_assignment/car_parts/dao/PartIMPL.java
@@ -1,4 +1,56 @@
 package com.car_parts_assignment.car_parts.dao;
 
-public class PartIMPL {
+
+import com.car_parts_assignment.car_parts.entity.Part;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public class PartIMPL implements PartDAO{
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public PartIMPL(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public List<Part> findAll() {
+        Session currentSession = entityManager.unwrap(Session.class);
+        Query<Part> myQuery = currentSession.createQuery("from Part");
+        return myQuery.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public Part findById(int theId) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        return currentSession.get(Part.class, theId);
+    }
+
+
+
+    @Override
+    @Transactional
+    public void saveOrUpdate(Part thePart) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        currentSession.saveOrUpdate(thePart);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int theId) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        Part myPart = currentSession.get(Part.class, theId);
+        currentSession.delete(myPart);
+    }
+
 }
