050ac00b1832d17be8f1d959a4fd2c7524524a48
bradfan <bradley.tomlinson@genspark.com>
2022-02-02 14:17:25 -0700

    Init Commit API Practice

---
+++ genspark/apipractice/src/test/java/com/songapi/apipractice/ApipracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songapi.apipractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApipracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
