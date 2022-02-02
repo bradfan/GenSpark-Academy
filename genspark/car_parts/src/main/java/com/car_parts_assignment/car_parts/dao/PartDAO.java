47e5bedfc252ccd8401825566f4ae4729d1d9393
bradfan <bradley.tomlinson@genspark.com>
2022-02-02 09:30:20 -0700

    Create PartDAO, car parts

--- genspark/car_parts/src/main/java/com/car_parts_assignment/car_parts/dao/PartDAO.java
+++ genspark/car_parts/src/main/java/com/car_parts_assignment/car_parts/dao/PartDAO.java
@@ -1,4 +1,12 @@
 package com.car_parts_assignment.car_parts.dao;
 
+import com.car_parts_assignment.car_parts.entity.Part;
+
+import java.util.List;
+
 public interface PartDAO {
+    List<Part> findAll();
+    Object findById(int theId);
+    void saveOrUpdate(Part thePart);
+    void deleteById(int theId);
 }
