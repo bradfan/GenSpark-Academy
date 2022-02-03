73d46e7d9c502194af64287e3448dc5f123eed8f
bradfan <bradley.tomlinson@genspark.com>
2022-02-03 08:02:14 -0700

    Debug, modify SongController, api practice

--- genspark/apipractice/src/main/java/com/songapi/apipractice/controller/SongController.java
+++ genspark/apipractice/src/main/java/com/songapi/apipractice/controller/SongController.java
@@ -20,23 +20,35 @@ public class SongController {
         this.songService = songService;
     }
 
+    //http://localhost:8080/retrieveAllSongs
     @GetMapping("/retrieveAllSongs")
     public List<Song> findAll() {
         return songService.findAll();
     }
 
+    //http://localhost:8080/retrieveById/1
     @GetMapping("/retrieveById/{songId}")
     public Object findById(@PathVariable int songId) {
         return songService.findById(songId);
     }
 
+    //http://localhost:8080/addSong
+    @PostMapping("/addSong")
+    public Song addSong(@RequestBody Song theSong) {
+        theSong.setId(0);
+        songService.saveOrUpdate(theSong);
+        return theSong;
+    }
+
+    //http://localhost:8080/updateSong
     @PutMapping("/updateSong")
     public Song updateSong(@RequestBody Song updateSong) {
         songService.saveOrUpdate(updateSong);
         return updateSong;
     }
 
-    @DeleteMapping("/deletePart/{songtId}")
+    //http://localhost:8080/deleteSong/1
+    @DeleteMapping("/deletePart/{songId}")
     public String deleteSong(@PathVariable int songId) {
         songService.deleteById(songId);
         return "Deleted song ID: " + songId;
