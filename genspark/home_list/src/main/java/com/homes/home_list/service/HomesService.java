2d8c61f08227736671fc42fa30c299d8df85db68
bradfan <bradley.tomlinson@genspark.com>
2022-02-04 14:52:26 -0700

    Create Service interface Homes

--- genspark/home_list/src/main/java/com/homes/home_list/service/HomesService.java
+++ genspark/home_list/src/main/java/com/homes/home_list/service/HomesService.java
@@ -1,4 +1,12 @@
 package com.homes.home_list.service;
 
+import com.homes.home_list.entity.Homes;
+
+import java.util.List;
+
 public interface HomesService {
+    List<Homes> findAll();
+    Object findById(int homesId);
+    void saveOrUpdate(Homes theHomes);
+    void deleteById(int HomesId);
 }
