34d7fae6cb099e6d5268c911ca4acb15fc4d654b
bradfan <bradley.tomlinson@genspark.com>
2022-02-04 08:41:45 -0700

    Create Game Service IMPL, api practice

--- genspark/apipractice/src/main/java/com/songapi/apipractice/service/GenreServiceIMPL.java
+++ genspark/apipractice/src/main/java/com/songapi/apipractice/service/GenreServiceIMPL.java
@@ -1,4 +1,31 @@
 package com.songapi.apipractice.service;
 
-public class GenreServiceIMPL {
+import com.songapi.apipractice.dao.GenreDao;
+import com.songapi.apipractice.entity.Genre;
+import com.songapi.apipractice.entity.Song;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class GenreServiceIMPL implements GenreService{
+
+    private final GenreDao genreDao;
+
+    @Autowired
+    public GenreServiceIMPL(GenreDao songDao) {this.genreDao = genreDao; }
+
+    @Override
+    public List<Genre> findAll() {return genreDao.findAll();}
+
+    @Override
+    public Object findById(int songId) {return genreDao.findById(genreId);}
+
+    @Override
+    public void saveOrUpdate(Genre theGenre) {
+        genreDao.saveOrUpdate(theGenre);}
+
+    @Override
+    public void deleteById(int genreId) {
+        genreDao.deleteById(genreId);}
+
 }
