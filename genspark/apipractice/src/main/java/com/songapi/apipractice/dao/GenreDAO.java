bf1ee17d372fac14feba5341a03dc44c227a1223
bradfan <bradley.tomlinson@genspark.com>
2022-02-04 08:45:44 -0700

    Create Genre DAO, api practice

--- genspark/apipractice/src/main/java/com/songapi/apipractice/dao/GenreDAO.java
+++ genspark/apipractice/src/main/java/com/songapi/apipractice/dao/GenreDAO.java
@@ -1,4 +1,15 @@
 package com.songapi.apipractice.dao;
 
+
+import com.songapi.apipractice.entity.Genre;
+
+
+import java.util.List;
+
+//DAO = Data Access Object
 public interface GenreDAO {
+    List<Genre> findAll();
+    Object findById(int genreId);
+    void saveOrUpdate(Genre theGenre);
+    void deleteById(int genreId);
 }
