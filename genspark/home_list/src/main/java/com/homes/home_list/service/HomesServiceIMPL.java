4cd6981c6763e82d360c7bdbc31abea0435ba55f
bradfan <bradley.tomlinson@genspark.com>
2022-02-04 14:57:11 -0700

    Create HomesService IMPL Homes

--- genspark/home_list/src/main/java/com/homes/home_list/service/HomesServiceIMPL.java
+++ genspark/home_list/src/main/java/com/homes/home_list/service/HomesServiceIMPL.java
@@ -1,4 +1,31 @@
 package com.homes.home_list.service;
 
-public class HomesServiceIMPL {
+
+import com.homes.home_list.dao.HomesDAO;
+import com.homes.home_list.entity.Homes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HomesServiceIMPL implements HomesService{
+    private final HomesDAO homesDao;
+
+    @Autowired
+    public HomesServiceIMPL(HomesDAO homesDao) {this.homesDao = homesDao; }
+
+    @Override
+    public List<Homes> findAll() {return homesDao.findAll();}
+
+    @Override
+    public Object findById(int homesId) {return homesDao.findById(homesId);}
+
+    @Override
+    public void saveOrUpdate(Homes theHomes) {homesDao.saveOrUpdate(theHomes);}
+
+    @Override
+    public void deleteById(int homesId) {homesDao.deleteById(homesId);}
+
+
 }
