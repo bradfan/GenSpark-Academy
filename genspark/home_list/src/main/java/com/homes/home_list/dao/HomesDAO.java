22e773e697c6c12ee73135783829905c0cf04ad4
bradfan <bradley.tomlinson@genspark.com>
2022-02-04 14:51:20 -0700

    Create DAO Homes

--- genspark/home_list/src/main/java/com/homes/home_list/dao/HomesDAO.java
+++ genspark/home_list/src/main/java/com/homes/home_list/dao/HomesDAO.java
@@ -1,4 +1,13 @@
 package com.homes.home_list.dao;
 
+import com.homes.home_list.entity.Homes;
+
+import java.util.List;
+
 public interface HomesDAO {
+    List<Homes> findAll();
+    Object findById(int homesId);
+    void saveOrUpdate(Homes theHomes);
+    void deleteById(int HomesId);
+
 }
