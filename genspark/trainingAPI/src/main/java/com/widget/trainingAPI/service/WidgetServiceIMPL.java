fee8f843810b7aebd8a4e455abb077834fcd0c7f
bradfan <bradley.tomlinson@genspark.com>
2022-02-07 08:30:23 -0700

    Create WidgetServiceIMPL, widget

---
+++ genspark/trainingAPI/src/main/java/com/widget/trainingAPI/service/WidgetServiceIMPL.java
@@ -0,0 +1,27 @@
+package com.widget.trainingAPI.service;
+
+import com.widget.trainingAPI.dao.WidgetDAO;
+import com.widget.trainingAPI.entity.Widget;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class WidgetServiceIMPL implements WidgetService{
+    private final WidgetDAO widgetDAO;
+
+    @Autowired
+    public WidgetServiceIMPL(WidgetDAO widgetDAO) {this.widgetDAO = widgetDAO;}
+
+    @Override
+    public List<Widget> findAll() {return widgetDAO.findAll();}
+
+    @Override
+    public Object findById(int widgetId) {return widgetDAO.findById(widgetId);}
+
+    @Override
+    public void saveOrUpdate(Widget theWidget) {widgetDAO.saveOrUpdate(theWidget);}
+
+    @Override
+    public void deleteById(int widgetId) {widgetDAO.deleteById(widgetId);}
+
+}
