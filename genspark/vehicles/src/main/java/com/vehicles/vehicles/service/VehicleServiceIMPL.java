6a637699c8b144617254c581faeba7e70176f848
bradfan <bradley.tomlinson@genspark.com>
2022-02-08 07:39:01 -0700

    Create Vehicle Service IMPL

---
+++ genspark/vehicles/src/main/java/com/vehicles/vehicles/service/VehicleServiceIMPL.java
@@ -0,0 +1,29 @@
+package com.vehicles.vehicles.service;
+
+import com.vehicles.vehicles.dao.VehicleDAO;
+import com.vehicles.vehicles.entity.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VehicleServiceIMPL {
+    private final VehicleDAO vehicleDAO;
+
+    @Autowired
+    public VehicleServiceIMPL(VehicleDAO vehicleDAO) {this.vehicleDAO  =vehicleDAO;}
+
+    @Override
+    public List<Vehicle> findAll() {return vehicleDAO.findAll();}
+
+    @Override
+    public Object findById(int vehicleId) {return vehicleDAO.findById(vehicleId);}
+
+    @Override
+    public void saveOrUpdate(Vehicle theVehicle) {vehicleDAO.saveOrUpdate(theVehicle);}
+
+    @Override
+    public void deleteById(int vehicleId) {vehicleDAO.deleteById(vehicleId);}
+    
+}
