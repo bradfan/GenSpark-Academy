7b13ffc8a6168531c5730d53ef56fc982cb1c671
bradfan <bradley.tomlinson@genspark.com>
2022-02-16 09:59:18 -0700

    Login IMPL complete

--- genspark/db/src/main/java/react_db/db/service/LoginServiceIMPL.java
+++ genspark/db/src/main/java/react_db/db/service/LoginServiceIMPL.java
@@ -1,4 +1,30 @@
 package react_db.db.service;
 
-public class LoginServiceIMPL {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import react_db.db.dao.LoginDao;
+import react_db.db.entity.Login;
+
+import java.util.List;
+
+@Service
+public class LoginServiceIMPL implements LoginService{
+    private final LoginDao loginDao;
+
+    @Autowired
+    public LoginServiceIMPL(LoginDao loginDao) {this.loginDao = loginDao;}
+
+    @Override
+    public List<Login> findAllLogin() {return loginDao.findAllLogin();}
+
+    @Override
+    public Object findByLoginId(int loginId) {return loginDao.findByLoginId(loginId);}
+
+    @Override
+    public void saveOrUpdateLogin(Login theLogin) {
+        loginDao.saveOrUpdateLogin(theLogin);}
+
+    @Override
+    public void deleteByLoginId(int loginId) {
+        loginDao.deleteByLoginId(loginId);}
 }
