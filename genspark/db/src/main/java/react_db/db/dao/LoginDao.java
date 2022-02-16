9bd77b3c2cb774a4ec48cb186cd3898ae8f47928
bradfan <bradley.tomlinson@genspark.com>
2022-02-16 09:52:11 -0700

    DAO IF complete

--- genspark/db/src/main/java/react_db/db/dao/LoginDao.java
+++ genspark/db/src/main/java/react_db/db/dao/LoginDao.java
@@ -1,4 +1,12 @@
 package react_db.db.dao;
 
+import react_db.db.entity.Login;
+
+import java.util.List;
+
 public interface LoginDao {
+    List<Login> findAllLogin();
+    Object findByLoginId(int loginID);
+    void saveOrUpdateLogin(Login theLogin);
+    void deleteByLoginId(int loginId);
 }
