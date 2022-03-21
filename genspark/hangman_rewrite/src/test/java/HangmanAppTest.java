a2c457ea4653ce2290b59b671d753ff7c74c2705
bradfan <bradley.tomlinson@genspark.com>
2022-03-21 09:56:52 -0700

    Generate JUnit tests for HangmanApp

---
+++ genspark/hangman_rewrite/src/test/java/HangmanAppTest.java
@@ -0,0 +1,57 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class HangmanAppTest {
+    HangmanApp app = new HangmanApp();
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void determineWord() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void userName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void retrieveDisplay() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void letterInput() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isCorrectLetter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void displayWord() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void outOfTries() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void success() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void points() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void highScore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
