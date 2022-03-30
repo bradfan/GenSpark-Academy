5ee1b22c47824aeb4bb82905fa208f5522e2c3ed
bradfan <bradley.tomlinson@genspark.com>
2022-03-30 08:23:45 -0700

    Copy and Paste CoffeeMachine from bugged JetBrains

---
+++ genspark/CoffeeMachineFinal/src/main/java/CoffeeMachine.java
@@ -0,0 +1,8 @@
+
+public class CoffeeMachine {
+
+    public static void main(String[] args) {
+        Machine machine = new Machine();
+        machine.onOff();
+    }
+}
---
+++ genspark/CoffeeMachineFinal/src/main/java/MachineState.java
@@ -0,0 +1,67 @@
+
+public class MachineState {
+    private int waterSupply;
+    private int milkSupply;
+    private int beanSupply;
+    private int cupSupply;
+    private int cash;
+
+    public MachineState(int waterSupply, int milkSupply, int beanSupply, int cupSupply, int cash) {
+        this.waterSupply = waterSupply;
+        this.milkSupply = milkSupply;
+        this.beanSupply = beanSupply;
+        this.cupSupply = cupSupply;
+        this.cash = cash;
+    }
+
+    public int getWaterSupply() {
+        return waterSupply;
+    }
+
+    public void setWaterSupply(int waterSupply) {
+        this.waterSupply = waterSupply;
+    }
+
+    public int getMilkSupply() {
+        return milkSupply;
+    }
+
+    public void setMilkSupply(int milkSupply) {
+        this.milkSupply = milkSupply;
+    }
+
+    public int getBeanSupply() {
+        return beanSupply;
+    }
+
+    public void setBeanSupply(int beanSupply) {
+        this.beanSupply = beanSupply;
+    }
+
+    public int getCupSupply() {
+        return cupSupply;
+    }
+
+    public void setCupSupply(int cupSupply) {
+        this.cupSupply = cupSupply;
+    }
+
+    public int getCash() {
+        return cash;
+    }
+
+    public void setCash(int cash) {
+        this.cash = cash;
+    }
+
+
+    public void printState() {
+        System.out.println("The coffee machine has:");
+        System.out.println(getWaterSupply() + " ml of water");
+        System.out.println(getMilkSupply() + " ml of milk");
+        System.out.println(getBeanSupply() + " g of coffee beans");
+        System.out.println(getCupSupply() + " disposable cups");
+        System.out.println("$" + getCash() + " of money");
+        System.out.println();
+    }
+}
