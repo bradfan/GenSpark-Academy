5065b79ef53694f29705ad0885481b4283f5475b
bradfan <bradley.tomlinson@genspark.com>
2022-04-18 09:26:39 -0700

    Create category service interface

--- genspark/budget/src/main/java/com/budget/budget/service/CategoryService.java
+++ genspark/budget/src/main/java/com/budget/budget/service/CategoryService.java
@@ -1,4 +1,12 @@
 package com.budget.budget.service;
 
+import com.budget.budget.entity.Category;
+
+import java.util.List;
+
 public interface CategoryService {
+    List<Category> findAll();
+    Object findById(int categoryId);
+    void saveOrUpdate(Category category);
+    void deleteById(int categoryId);
 }
