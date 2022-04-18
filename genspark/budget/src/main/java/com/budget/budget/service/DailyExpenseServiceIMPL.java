b45e83bbdd9bfa523441714c54edc8fada2bddd1
bradfan <bradley.tomlinson@genspark.com>
2022-04-18 07:34:06 -0700

    Create daily expense service impl budget

--- genspark/budget/src/main/java/com/budget/budget/service/DailyExpenseServiceIMPL.java
+++ genspark/budget/src/main/java/com/budget/budget/service/DailyExpenseServiceIMPL.java
@@ -1,10 +1,36 @@
 package com.budget.budget.service;
 
-import javax.persistence.*;
+import com.budget.budget.dao.DailyExpenseDao;
+import com.budget.budget.entity.DailyExpense;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
 
-public class DailyExpenseServiceIMPL {
+@Service
+public class DailyExpenseServiceIMPL implements DailyExpenseService {
 
+    // Injecting the daily expense dao layer
+    private final DailyExpenseDao dailyExpenseDao;
 
-
+    @Autowired
+    public DailyExpenseServiceIMPL(DailyExpenseDao dailyExpenseDao) {
+        this.dailyExpenseDao = dailyExpenseDao;
+    }
+    @Override
+    public List<DailyExpense> findAll() {
+        return dailyExpenseDao.findAll();
+    }
+    @Override
+    public Object findById(int dailyExpenseId) {
+        return dailyExpenseDao.findById(dailyExpenseId);
+    }
+    @Override
+    public void saveOrUpdate(DailyExpense dailyExpense) {
+        dailyExpenseDao.saveOrUpdate(dailyExpense);
+    }
+    @Override
+    public void deleteById(int dailyExpenseId) {
+        dailyExpenseDao.deleteById(dailyExpenseId);
+    }
 }
