7f8c1d04af6ea3654a6b70dc6a91776ce7b03610
bradfan <bradley.tomlinson@genspark.com>
2022-04-18 07:16:53 -0700

    Create daily expense interface budget

--- genspark/budget/src/main/java/com/budget/budget/service/DailyExpenseService.java
+++ genspark/budget/src/main/java/com/budget/budget/service/DailyExpenseService.java
@@ -1,4 +1,12 @@
 package com.budget.budget.service;
 
+import com.budget.budget.entity.DailyExpense;
+
+import java.util.List;
+
 public interface DailyExpenseService {
+    List<DailyExpense> findAll();
+    Object findById(int dailyExpenseId);
+    void saveOrUpdate(DailyExpense dailyExpense);
+    void deleteById(int dailyExpenseId);
 }
