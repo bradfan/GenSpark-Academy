5cdb88a65f67fbdc3d44b2b5849604f3e7e1951e
bradfan <bradley.tomlinson@genspark.com>
2022-04-18 09:27:50 -0700

    Create Category DAO

--- genspark/budget/src/main/java/com/budget/budget/dao/CategoryDao.java
+++ genspark/budget/src/main/java/com/budget/budget/dao/CategoryDao.java
@@ -1,4 +1,12 @@
 package com.budget.budget.dao;
 
+import com.budget.budget.entity.Category;
+
+import java.util.List;
+
 public interface CategoryDao {
+    List<Category> findAll();
+    Object findById(int categoryId);
+    void saveOrUpdate(Category category);
+    void deleteById(int categoryId);
 }
