d2ee87dc1ff5bd8664ef7c6921c80fb0374e5c95
bradfan <bradley.tomlinson@genspark.com>
2022-04-18 07:18:30 -0700

    Create daily expense dao interface budget

--- genspark/budget/src/main/java/com/budget/budget/dao/DailyExpenseDao.java
+++ genspark/budget/src/main/java/com/budget/budget/dao/DailyExpenseDao.java
@@ -1,4 +1,14 @@
 package com.budget.budget.dao;
 
+import com.budget.budget.entity.DailyExpense;
+
+import java.util.List;
+
 public interface DailyExpenseDao {
+    public interface DailyExpenseService {
+        List<DailyExpense> findAll();
+        Object findById(int dailyExpenseId);
+        void saveOrUpdate(DailyExpense dailyExpense);
+        void deleteById(int dailyExpenseId);
+    }
 }
