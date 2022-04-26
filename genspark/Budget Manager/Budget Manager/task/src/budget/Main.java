ccb0c807f012ff3e4ccb92c68952d98710d5c5a0
bradfan <bradley.tomlinson@genspark.com>
2022-04-26 06:59:39 -0700

    JetBrains

--- genspark/Budget Manager/Budget Manager/task/src/budget/Main.java
+++ genspark/Budget Manager/Budget Manager/task/src/budget/Main.java
@@ -1,7 +1,33 @@
 package budget;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class Main {
+    public static List<Integer> intro() {
+        System.out.println("Please enter your purchases \n" +
+                "as <Description> followed by the $ sign \n" +
+                "followed by the amount spent. \n" +
+                "Use Ctrl+z to enter a new purchase.");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        List<Integer> purchaseList = new ArrayList<>();
+
+        while (sc.hasNext()) {
+            System.out.println(input);
+            String[] collector = input.split(" ");
+            purchaseList.add(Integer.valueOf(collector[collector.length - 1]));
+        }
+        System.out.println(purchaseList);
+        return purchaseList;
+    }
+
+
     public static void main(String[] args) {
-        // write your code here
+        intro();
+
     }
 }
